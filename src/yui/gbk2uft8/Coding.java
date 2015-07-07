14724066cbef7e1b2897c6d81e0ac5382b8297c6
Crysty <392420488@qq.com>
2015-07-07 08:04:46 +0800

    refactor

---
+++ src/yui/gbk2uft8/Coding.java
@@ -0,0 +1,36 @@
+package yui.gbk2uft8;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Coding {
+
+	public static void coding(String resource, String target) {
+		BufferedReader reader = null;
+		PrintWriter writer = null;
+		try {
+			FileInputStream in = new FileInputStream(resource);
+
+			reader = new BufferedReader(new InputStreamReader(in, "GBK"));
+			writer = new PrintWriter(target, "UTF-8");
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				writer.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			writer.close();
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
--- src/yui/gbk2uft8/Main.java
+++ src/yui/gbk2uft8/Main.java
@@ -1,16 +1,11 @@
 package yui.gbk2uft8;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 
 // GBK编码转换成UTF-8编码
 public class Main {
 
-	private static String rootPath = "G:\\Projects\\JavaSE\\Yui\\src";
+	private static String rootPath = "G:\\Projects\\JavaSE\\Yui-net\\src";
 
 	public static void start(String path) {
 		File rootFile = new File(path);
@@ -22,35 +17,13 @@ public class Main {
 			if (file.isDirectory()) {
 				start(file.getPath());
 			} else {
-				BufferedReader reader = null;
-				PrintWriter writer = null;
-				try {
-					FileInputStream in = new FileInputStream(file);
-
-					String p = file.getParent().substring(rootPath.length());
-					File targetFile = new File(rootPath + "-bk/" + p);
-					if (!targetFile.exists()) {
-						targetFile.mkdirs();
-					}
-					reader = new BufferedReader(
-							new InputStreamReader(in, "GBK"));
-					writer = new PrintWriter(targetFile + "/" + file.getName(),"UTF-8");
-					String line = null;
-					while ((line = reader.readLine()) != null) {
-						writer.println(line);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} finally {
-					writer.close();
-					if (reader != null) {
-						try {
-							reader.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
+				String p = file.getParent().substring(rootPath.length());
+				File targetFile = new File(rootPath + "-bk/" + p);
+				if (!targetFile.exists()) {
+					targetFile.mkdirs();
 				}
+				Coding.coding(file.toString(),
+						targetFile + "/" + file.getName());
 			}
 		}
 	}
