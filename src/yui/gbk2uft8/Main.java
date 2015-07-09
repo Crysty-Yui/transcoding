bd7ca5ebaa4e56098a079279a384429b1aa81819
dell <dell@dell028>
2015-07-09 09:19:06 +0800

    改命令行方式

--- src/yui/gbk2uft8/Main.java
+++ src/yui/gbk2uft8/Main.java
@@ -1,6 +1,7 @@
 package yui.gbk2uft8;
 
 import java.io.File;
+import java.util.Scanner;
 
 // GBK编码转换成UTF-8编码
 public class Main {
@@ -22,13 +23,20 @@ public class Main {
 				if (!targetFile.exists()) {
 					targetFile.mkdirs();
 				}
-				Coding.coding(file.toString(),
-						targetFile + "/" + file.getName());
+				if (file.getName().endsWith(".java")) {
+					Coding.coding(file.toString(),
+							targetFile + "/" + file.getName());
+				} else {
+					file.renameTo(new File(targetFile + "/" + file.getName()));
+				}
 			}
 		}
 	}
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		rootPath = scan.nextLine();
 		start(rootPath);
+		scan.close();
 	}
 }
